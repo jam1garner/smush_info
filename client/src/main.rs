3979fba581a2273de103cdf48ab1649f020a49f3
jam1garner <8260240+jam1garner@users.noreply.github.com>
2020-06-06 01:12:39 -0400

    Add discord rich presence to client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,15 @@
-use smush_discord_shared::Info;
+use smush_discord_shared::{Info, Stage};
 use std::net::{TcpStream, IpAddr};
 use std::io::{BufRead, BufReader};
+use rustcord::{Rustcord, EventHandlers, User, RichPresenceBuilder, RichPresence};
+
+pub struct Handlers;
+
+impl EventHandlers for Handlers {
+    fn ready(user: User) {
+        println!("User {}#{} logged in...", user.username, user.discriminator);
+    }
+}
 
 const IP_ADDR_FILE: &str = "ip_addr.txt";
 
@@ -27,10 +36,50 @@ fn get_info(bytes: &[u8]) -> Info {
     serde_json::from_slice(bytes).unwrap()
 }
 
+fn stage_to_image_key(stage: Stage) -> String {
+    let name = format!("{:?}", stage);
+    let name = name.trim_start_matches('_');
+    if name.starts_with("Battle_") {
+        &name["Battle_".len()..]
+    } else if name.starts_with("End_") {
+        &name["End_".len()..]
+    } else {
+        name
+    }.to_owned()
+}
+
+fn info_to_presence(info: &Info) -> RichPresence {
+    RichPresenceBuilder::new()
+        .state("In Match")
+        .details(
+            &format!(
+                "{} {} - {} {}",
+                info.players[0].character(),
+                info.players[0].stocks(),
+                info.players[1].stocks(),
+                info.players[1].character(),
+            )
+        )
+        .large_image_key(&stage_to_image_key(info.stage()))
+        .large_image_text(&info.stage().into_normal().to_string())
+        .build()
+}
+
 fn main() {
     let packets = BufReader::new(TcpStream::connect((get_home_ip(), 4242u16)).unwrap()).split(b'\n');
 
+    let discord = Rustcord::init::<Handlers>("718317785016565790", true, None).unwrap();
+
+
     for packet in packets {
-        println!("{:#?}", get_info(&packet.unwrap()));
+        let info = get_info(&packet.unwrap());
+        let presence = info_to_presence(&info);
+
+        let res = discord.update_presence(presence);
+        if let Err(error) = res {
+            dbg!(error);
+        }
+
+        discord.run_callbacks();
     }
 }
